d27c41f85ac2fed19cc318e58384ba4c71ab920c
MathiasLintner <se231327@fhstp.ac.at>
2025-08-26 07:33:02 +0200

    Add files via upload

--- models/Fahrt.js
+++ models/Fahrt.js
@@ -7,7 +7,11 @@ const Fahrt = sequelize.define('Fahrt', {
   ziel:    { type: DataTypes.STRING, allowNull: false },
   plaetze: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 1 },
   datum:   { type: DataTypes.DATEONLY, allowNull: true },
-  preis:   { type: DataTypes.FLOAT, allowNull: true }
+  uhrzeit: { type: DataTypes.STRING, allowNull: true },
+  distanz: { type: DataTypes.FLOAT, allowNull: true },
+  preis:   { type: DataTypes.FLOAT, allowNull: true },
+  // NEU: Gebäck erlaubt?
+  gebaeckErlaubt: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
 });
 
 module.exports = Fahrt;
